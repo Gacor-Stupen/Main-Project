7542b89327a7f3b1d2795d553e916867e2969448
oMrPuPa <0rafipati0@gmail.com>
2026-05-23 15:37:02 +0700

    memisahkan folder frontEnd ke cabagn frontend dan menambah fitur untuk isi data Pribadi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage'
 import HomePage from './pages/HomePage';
 import LandingPage from './pages/LandingPage';
+import ResignForm from './pages/ResignForm';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/form" element={<ResignForm />} />
       </Routes>
     </Router>
   );
