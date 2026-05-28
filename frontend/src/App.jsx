015050971873820933806b1e88f9e6336885c395
oMrPuPa <0rafipati0@gmail.com>
2026-05-29 00:05:39 +0700

    fixing homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
-import FinancialPage from './pages/FinancialPage';
+// import FinancialPage from './pages/FinancialPage';
 import AnalyzePage from './pages/AnalyzePage';
 import ResultPage from './pages/ResultPage';
 import HistoryPage from './pages/HistoryPage';
-import FinancialResultPage from './pages/FinancialResultPage';
+// import FinancialResultPage from './pages/FinancialResultPage';
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/financial" element={<FinancialPage />} />
-        <Route path="/result/financial" element={<FinancialResultPage />} />
+        {/* <Route path="/financial" element={<FinancialPage />} /> */}
+        {/* <Route path="/result/financial" element={<FinancialResultPage />} /> */}
         <Route path="/analyze" element={<AnalyzePage />} />
         <Route path="/result" element={<ResultPage />} /> 
         <Route path="/history" element={<HistoryPage />} />
