ddcb626cb90614cbd24572b716bac7dd354c9562
oMrPuPa <0rafipati0@gmail.com>
2026-05-31 22:26:06 +0700

    copy dari branch resignajadulu_v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage'
+import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
-import LandingPage from './pages/LandingPage';
-import ResignForm from './pages/ResignForm';
+import AnalyzePage from './pages/AnalyzePage';
+import ResultPage from './pages/ResultPage';
+import SkillPage from './pages/SkillPage';
+import SkillResultPage from './pages/SkillResultPage';
+import ProfilePage from './pages/ProfilePage';
+import HistoryDetailPage from './pages/HistoryDetailPage';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/form" element={<ResignForm />} />
+        <Route path="/analyze" element={<AnalyzePage />} />
+        <Route path="/result" element={<ResultPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/skill" element={<SkillPage />} />
+        <Route path="/skill-result" element={<SkillResultPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/history/:careerId" element={<HistoryDetailPage />} />
+        
       </Routes>
     </Router>
   );
